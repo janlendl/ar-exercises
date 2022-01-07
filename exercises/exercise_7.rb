416f70181f1d045b50fa4d50d15abb1fa2ec84eb
Lendl Peralta <janlendl@gmail.com>
2022-01-07 01:13:21 +0000

    Complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,27 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, :last_name, :store, presence: true
+  validates :hourly_rate, numericality: { in: 40..200 }
+end
+
+class Store
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :apparel_check
+
+
+  def apparel_check
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:store, "The store must carry at least one type of apparel")
+    end
+  end
+end
+
+puts "Enter store name: "
+store_name = gets.chomp
+
+store = Store.create(name: store_name, annual_revenue: 10000, mens_apparel: false, womens_apparel: false)
+
+puts store.errors.full_messages
